780dc6808561a166ff88953be62d390d3070e5f9
Rapadapl <d.lysyenko@ya.ru>
2021-09-17 11:16:45 +1000

    wrapper

--- src/main/kotlin/com/justai/jaicf/profit/AliceSkill.kt
+++ src/main/kotlin/com/justai/jaicf/profit/AliceSkill.kt
@@ -4,10 +4,10 @@ import com.justai.jaicf.BotEngine
 import com.justai.jaicf.activator.catchall.CatchAllActivator
 import com.justai.jaicf.activator.event.BaseEventActivator
 import com.justai.jaicf.channel.yandexalice.activator.AliceIntentActivator
-import com.justai.jaicf.profit.scenario.MainScenario
+import com.justai.jaicf.profit.scenario.`MainScenario(clone)`
 
 val skill = BotEngine(
-    model = MainScenario.model,
+    model = `MainScenario(clone)`.model,
     activators = arrayOf(
         AliceIntentActivator,
         BaseEventActivator,
